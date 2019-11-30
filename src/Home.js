25f65861400172e6ec11bb59da58de08c1083102
Iain Walmsley <iain.walmsley@gmail.com>
2019-11-30 22:34:17 +0000

    text changes

--- src/Home.js
+++ src/Home.js
@@ -29,15 +29,16 @@ export default function Home() {
       </div>
       <div className={classes.description}>
         <Typography className={classes.description}>
-          Based in the heart of Lancaster, Lancashire, we welcome alll players
-          to come and join us. Be it for casual or competitive games, discussion of
-          theory or general socialization, there is room for everyone to play at our club.
+          Based in the heart of Lancaster, UK, our historic club which dates back
+          to 1895 welcomes players of all abilities to come and enjoy a game of chess. Be 
+          it for short, long, casual or competitive games there's something for everyone
+          here.
         </Typography>
         <Typography className={classes.description}>
-          Featuring longstanding and new members of all abilities, the club
-          meets twice weekly to engage in chess ranging from the yearly round robin
-          Club Championship (90 minutes), to the rapid play Club Championship, to the yearly
-          Blitz tournament. 
+          The club meets twice weekly and over the course of the year highlights include
+          the round robin club championship (90 minutes), to the rapid play championship,
+          as well as occasional blitz chess tournaments. And for players who prefer to 
+          play casual games club nights always offer the opportunity to have a game.
         </Typography>
         <Typography className={classes.description}>
           If this sounds good please see the information for club nights below - we look 
