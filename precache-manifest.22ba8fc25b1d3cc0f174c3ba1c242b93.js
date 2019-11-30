e7f8c390b9108efa9b13f3bae9120e97a0077c0a
Iain Walmsley <iain.walmsley@gmail.com>
2019-11-30 12:48:59 +0000

    Updates

---
+++ precache-manifest.22ba8fc25b1d3cc0f174c3ba1c242b93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6ff68439a3b6e06e9475db02ceff85e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1466d0974b74bdef0207",
+    "url": "/static/css/main.49dfb6b8.chunk.css"
+  },
+  {
+    "revision": "d73023bfebe6a171df37",
+    "url": "/static/js/2.3e9b07ca.chunk.js"
+  },
+  {
+    "revision": "1466d0974b74bdef0207",
+    "url": "/static/js/main.c992968c.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  }
+]);
