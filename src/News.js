e4064f20a762fc46ec6b67f5db2fd192138c38d9
Iain Walmsley <iain.walmsley@gmail.com>
2019-12-02 21:16:29 +0000

    move to use markdown for posts

--- src/News.js
+++ src/News.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import ArticleOne from './newsArticles/ArticleOne';
 import theme from './theme';
+import ArticleTwo from './newsArticles/ArticleTwo';
 
 const useStyles = makeStyles({
     container: {
@@ -22,6 +23,7 @@ export default function News() {
     <div className={classes.container}>
       <Typography className={classes.title} variant="h1">News</Typography>
       <ArticleOne/>
+      <ArticleTwo/>
     </div>
   );
 }
---
+++ src/newsArticles/ArticleTwo.js
@@ -0,0 +1,34 @@
+import React, { Component} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ReactMarkdown from 'react-markdown';
+import Article from './ArticleTwo.md';
+
+const useStyles = makeStyles({
+    container: {
+      maxWidth: '700px',
+      display: 'inline-block',
+      paddingLeft: '2%',
+      paddingRight: '2%',
+      paddingTop: '20px',
+      paddingBottom: '20px',
+    },
+  });
+
+class ArticleTwo extends Component {
+  constructor() {
+    super();
+    this.state = { markdown: '' };
+  }
+
+  componentWillMount() {
+    // Get the contents from the Markdown file and put them in the React state, so we can reference it in render() below.
+    fetch(Article).then(res => res.text()).then(text => this.setState({ markdown: text }));
+  }
+
+  render() {
+    const { markdown } = this.state;
+    return <ReactMarkdown source={markdown} />;
+  }
+}
+
+export default ArticleTwo;
