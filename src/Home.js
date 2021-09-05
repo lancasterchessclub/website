ff3f7f3e696752e9ad1d309ccd54580b3c0ee5a2
iwalmsley <iain.walmsley@gmail.com>
2021-09-05 10:06:36 +0000

    Deploying to gh-pages from @ lancasterchessclub/website@cee25f1ce5e06b7c2b318fbb7bd1af044edb644f 🚀

---
+++ src/Home.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import theme from './theme';
+
+const useStyles = makeStyles({
+    center: {
+      width: '100%',
+      textAlign: 'center',
+    },
+    title: {
+      paddingTop: '20px',
+      fontSize: theme.typography.title.fontSize,
+    },
+    description: {
+      width: '100%',
+      paddingTop: '20px',
+      paddingBottom: '20px',
+      maxWidth: '700px',
+      display: 'inline-block',
+    },  
+  });
+
+export default function Home() {
+    const classes = useStyles();
+    return (
+      <div className={classes.center}>
+      <div className={classes.title}>
+        <Typography className={classes.title} variant="h1">A Friendly Chess Club in the Heart of Lancaster</Typography>
+      </div>
+      <div className={classes.description}>
+        <h4>Update 05/09/21: The new season is now happily approaching. The resumption of activity
+          will take place on Tuesday September 21st at 7pm with the AGM followed by our first
+          clubnight. We look forward to seeing everyone again then.
+        </h4>
+        <Typography className={classes.description}>
+          Based in the heart of Lancaster, UK, our historic chess club, which dates back
+          to 1895, welcomes players of all ages and abilities to come and enjoy a game of chess.
+          Be it for short, long, casual or competitive games there's something for everyone.
+        </Typography>
+        <Typography className={classes.description}>
+          We meet weekly, and over the course of the year, highlights range from
+          the round robin club chess championship (90 minutes), to the rapid play championship,
+          as well as occasional blitz chess tournaments. And for players who prefer to 
+          play casual games, club nights always offer the opportunity to have a game.
+        </Typography>
+        <Typography className={classes.description}>
+          If this sounds good, please see the information for club nights below - please
+          do get in touch via e-mail and we look forward to welcoming you soon!
+        </Typography>
+      </div>
+      </div>
+    );
+  }
---
+++ src/News.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import theme from './theme';
+import Article from './newsArticles/ArticleMD';
+
+const useStyles = makeStyles({
+    container: {
+      width: '70%',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+    title: {
+      paddingTop: '20px',
+      textAlign: 'center',
+      fontSize: theme.typography.title.fontSize,
+    },
+  });
+
+export default function News() {
+  const classes = useStyles();
+
+  const files = [
+    './9.md',
+    './8.md',
+    './7.md',
+    './6.md',
+    './5.md',
+    './4.md',
+    './3.md',
+    './2.md',
+    './1.md',
+  ];
+
+  return (
+    <div className={classes.container}>
+      <Typography className={classes.title} variant="h1">News</Typography>
+      {files.map(file => <Article path={file} key={file}/>)}
+    </div>
+  );
+}
