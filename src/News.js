7954edb65d7a53b390f48c06cac35e873f0935e1
Iain Walmsley <iain.walmsley@gmail.com>
2022-02-15 13:19:05 +0000

    feat: add basic pagination

--- src/News.js
+++ src/News.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import theme from './theme';
@@ -17,30 +18,70 @@ const useStyles = makeStyles({
     },
   });
 
+const files = [
+  './14.md',
+  './13.md',
+  './12.md',
+  './11.md',
+  './10.md',
+  './9.md',
+  './8.md',
+  './7.md',
+  './6.md',
+  './5.md',
+  './4.md',
+  './3.md',
+  './2.md',
+  './1.md',
+];
+
+function Items({ currentItems }) {
+  return (
+    <>
+      {currentItems.map(item => <Article path={item} key={item}/>)}
+    </>
+  );
+}
+
+function PaginatedItems({ itemsPerPage }) {
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(files.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(files.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % files.length;
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <>
+      <Items currentItems={currentItems} />
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="Next"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="Previous"
+        renderOnZeroPageCount={null}
+      />
+    </>
+  );
+}
+
 export default function News() {
   const classes = useStyles();
-
-  const files = [
-    './14.md',
-    './13.md',
-    './12.md',
-    './11.md',
-    './10.md',
-    './9.md',
-    './8.md',
-    './7.md',
-    './6.md',
-    './5.md',
-    './4.md',
-    './3.md',
-    './2.md',
-    './1.md',
-  ];
 
   return (
     <div className={classes.container}>
       <Typography className={classes.title} variant="h1">News</Typography>
-      {files.map(file => <Article path={file} key={file}/>)}
+      <PaginatedItems itemsPerPage={4} />
     </div>
   );
 }
